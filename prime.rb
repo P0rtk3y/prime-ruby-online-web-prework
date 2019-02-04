4e3a86938009be8471b4ae509d05c30490c50f8a
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-04 06:32:03 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime?(integer)
   if integer > 1
     (2...integer).each do |divisor|
       if integer % divisior == 0 
-        return false
+        false
       else true
       end
     end
